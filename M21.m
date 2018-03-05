cdfefa914852bdae64368d357ebe73f6b981c3e8
Vincent C Bintang <bintangvincent@gmail.com>
2018-03-05 12:50:33 +0800

    Update M21.m

--- M21.m
+++ M21.m
@@ -1,4 +1,5 @@
 maxCount = 10; % number of points to include
+
 for (count = 1:maxCount)
 x(count) = maxCount/2-count; % count to x value
 y(count) = (sin(x(count))/x(count))^2; % calc function
@@ -6,3 +7,5 @@ y(count) = (sin(x(count))/x(count))^2; % calc function
 fprintf('x= %+2.2f y=%+1.3f\n', x(count), y(count));
 %2.2f means showing 2 floating number in fornt of decimal dan 2 number behind it 
 end
+
+plot(x,y);
